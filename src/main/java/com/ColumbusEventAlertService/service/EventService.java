952f645e404a91bbd115f1f5f824b463ed2b4396
Timothy Hart <timothyhart6@gmail.com>
2024-04-09 16:14:34 -0400

    Adding call to Twilio to send message to phone number

---
+++ src/main/java/com/ColumbusEventAlertService/service/EventService.java
@@ -0,0 +1,19 @@
+package com.ColumbusEventAlertService.service;
+
+import com.ColumbusEventAlertService.secrets.TwilioSecrets;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+
+public class EventService {
+    public Message sendNationwideEvent(String phoneNumber, String messageText) {
+        Twilio.init(TwilioSecrets.SID.getValue(), TwilioSecrets.AUTH_TOKEN.getValue());
+
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber(phoneNumber),
+                new com.twilio.type.PhoneNumber(TwilioSecrets.TWILIO_PHONE_NUMBER.getValue()),
+                messageText)
+        .create();
+
+        return message;
+    }
+}
