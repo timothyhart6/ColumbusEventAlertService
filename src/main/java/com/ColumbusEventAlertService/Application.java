f02548a283db6c5c6b92af35ae617d8d9f326535
Timothy Hart <timothyhart6@gmail.com>
2024-05-22 22:21:52 -0400

    removed unnecessary code

--- src/main/java/com/ColumbusEventAlertService/Application.java
+++ src/main/java/com/ColumbusEventAlertService/Application.java
@@ -3,14 +3,11 @@ package com.ColumbusEventAlertService;
 import com.ColumbusEventAlertService.columbusEvents.NationwideArenaEvents;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		NationwideArenaEvents nationwideArenaEvents = new NationwideArenaEvents();
-		nationwideArenaEvents.getUpcomingEvent();
 	}
 }
