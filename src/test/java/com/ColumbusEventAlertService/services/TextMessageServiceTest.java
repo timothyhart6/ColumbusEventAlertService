f196e60d87fa1f2e89a29236ae76711dd22acfbd
Timothy Hart <timothyhart6@gmail.com>
2024-06-24 22:07:41 -0400

    fix broken TextMessageServiceTest - No code change but test somehow broke

--- src/test/java/com/ColumbusEventAlertService/services/TextMessageServiceTest.java
+++ src/test/java/com/ColumbusEventAlertService/services/TextMessageServiceTest.java
@@ -9,38 +9,36 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
+import org.mockito.ArgumentCaptor;
+
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TextMessageServiceTest {
+    @InjectMocks
+    private TextMessageService textMessageService;
     @Mock
     private TwilioService twilioService;
     @Mock
     private EventsUtil eventsUtil;
-    @InjectMocks
-    private TextMessageService textMessageService;
+
     ArrayList<Event> events;
-    Event event;
 
     @BeforeEach
     public void setup() {
         events = new ArrayList<>();
-        event = new Event();
-
     }
 
     @Test
     public void testSendTodaysEvents() {
-        event.setLocationName("Home");
-        event.setEventName("TV");
-        event.setTime("7:00PM");
-        events.add(event);
-        String expectedMessage = "TODAY'S EVENTS:\n Home:\n  TV at 7:00PM\n";
-        when(eventsUtil.getTodaysEvents()).thenReturn(events);
 
         textMessageService.sendTodaysEvents();
 
-        verify(twilioService).sendTwilioText(anyString(), anyString(), eq(expectedMessage));
+        ArgumentCaptor<String> fromCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> toCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> messageCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(twilioService).sendTwilioText(fromCaptor.capture(), toCaptor.capture(), messageCaptor.capture());
     }
 
     @Test
