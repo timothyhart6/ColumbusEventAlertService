1e478af1db1e7ca3aaae42a03666d34573a04464
Timothy Hart <timothyhart6@gmail.com>
2024-09-02 17:14:21 -0400

    Testing deploy

--- src/main/java/com/ColumbusEventAlertService/Application.java
+++ src/main/java/com/ColumbusEventAlertService/Application.java
@@ -1,13 +1,15 @@
 package com.ColumbusEventAlertService;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
 	public static void main(String[] args) {
-		//create a diff
+		log.info("Application starting");
 		SpringApplication.run(Application.class, args);
 	}
 }
