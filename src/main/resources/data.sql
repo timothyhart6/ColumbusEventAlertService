19f2888d17a37e7643be4037af481b75d7765761
Timothy Hart <timothyhart6@gmail.com>
2024-11-26 20:17:40 -0500

    Create events table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,10 @@
-CREATE TABLE countries (
-    id varchar,
-    name varchar
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    event TEXT NOT NULL,
+    location TEXT NOT NULL,
+    date DATE NOT NULL,
+    start_time TIME,
+    end_time TIME
 );
 INSERT INTO countries (id, name) VALUES (1, 'USA');
 INSERT INTO countries (id, name) VALUES (2, 'France');
