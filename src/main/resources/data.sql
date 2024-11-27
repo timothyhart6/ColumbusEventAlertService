53dc570f84bf61fb96d82cd4ae8724101ca76666
Timothy Hart <timothyhart6@gmail.com>
2024-11-26 20:18:27 -0500

    Insert remaining 2024 events into events table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,8 +6,7 @@ CREATE TABLE events (
     start_time TIME,
     end_time TIME
 );
-INSERT INTO countries (id, name) VALUES (1, 'USA');
-INSERT INTO countries (id, name) VALUES (2, 'France');
-INSERT INTO countries (id, name) VALUES (3, 'Brazil');
-INSERT INTO countries (id, name) VALUES (4, 'Italy');
-INSERT INTO countries (id, name) VALUES (5, 'Canada');
+
+INSERT INTO events (event, location, date, start_time) VALUES ('OSU VS. Michigan', 'Ohio Stadium', '2024-10-26', '12:00');
+INSERT INTO events (event, location, date, start_time, end_time) VALUES ('Hops on High', 'Short North', '2024-12-07','12:00', '20:00');
+INSERT INTO events (event, location, date, start_time) VALUES ('Village Lights', 'German Village', '2024-12-08', '21:00');
